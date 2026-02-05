55e3d3ea9df61603a64219abee2cac8ec36027d8
Rusbak <mikkel.rusbak@gmail.com>
2026-02-05 14:03:10 +0100

    Package Test

---
+++ test_install.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib
+import scipy
+import numba
+import pytest
+import pytest_cov
+import dask
+import distributed
+
+print(np.__version__)
+print(matplotlib.__version__)
+print(scipy.__version__)
+print(numba.__version__)
+print(pytest.__version__)
+print(pytest_cov.__version__)
+print(dask.__version__)
+print(distributed.__version__)
