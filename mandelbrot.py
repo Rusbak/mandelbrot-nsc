c78d14b26335b271ad98b20206cee9de3b7be7f0
Rusbak <mikkel.rusbak@gmail.com>
2026-02-18 19:37:38 +0100

    Made mandelbrot_point function

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,20 +1,27 @@
-"""
-Mandelbrot Set Generator
-Author: [Your Name]
-Course: Numerical Scientific Computing 2026
-"""
-def f(x):
-    """
-    Example function.
-    
-    Parameters
-    ---------
-    x : float
-    Input value
-    Returns
-    ------
-    float
-    Output value
-    """
-    # TODO: Implement the algorithm
-pass
+# Naive Python Implementation
+
+def mandelbrot_point(x, y, max_iterations, bound):
+    complex_number = 1j
+    c = x + y * complex_number
+    z = 0
+    result = None
+
+    for iteration in range(max_iterations):
+        if(abs(z) >= bound):
+            result = iteration
+            break
+        else:
+            z = z**2 + c
+    else: # this is only called if the for loop never 'breaks'
+        result = max_iterations
+
+    return result
+
+x = 0
+y = 0
+max_iterations = 100
+bound = 2
+
+point = mandelbrot_point(x, y, max_iterations, bound)
+
+print(f'{point=}')
