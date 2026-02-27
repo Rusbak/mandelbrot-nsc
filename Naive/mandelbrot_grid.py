98767205367a315ea95d932380a7ecfa050d4d54
Rusbak <mikkel.rusbak@gmail.com>
2026-02-27 17:38:44 +0100

    Redid cProfiling
    the cProfiler made a wrong function call
    also now storing the stats in a .txt

--- Naive/mandelbrot_grid.py
+++ Naive/mandelbrot_grid.py
@@ -7,10 +7,11 @@ def compute_mandelbrot_grid(x_region, y_region, max_iterations, bound, power):
 
     for y_value in y_region:
         row = []
+
         for x_value in x_region:
             c = complex(x_value, y_value)
             z = 0
-            
+
             for iteration in range(max_iterations):
                 if(abs(z) >= bound):
                     row.append(iteration)
@@ -18,7 +19,7 @@ def compute_mandelbrot_grid(x_region, y_region, max_iterations, bound, power):
                 else: 
                     z = z**power + c
             else: # this is only called if the for loop never 'breaks'
-                row.append(0)
+                row.append(max_iterations)
         mandelbrot_array.append(row)
 
     return mandelbrot_array
@@ -36,17 +37,19 @@ max_iterations = 100
 bound = 2
 power = 2
 
-# test time of computation
-start_time = time.time()
-mandelbrot_array = compute_mandelbrot_grid(x_region, y_region, max_iterations, bound, power)
-test_time = time.time() - start_time
-print(f'Computation took {test_time:.3f} seconds!')
+if __name__ == '__main__':
+    # test time of computation
+    start_time = time.perf_counter()
+    mandelbrot_array = compute_mandelbrot_grid(x_region, y_region, max_iterations, bound, power)
+    test_time = time.perf_counter() - start_time
+    print(f'Computation took {test_time:.4f} seconds!')
 
-ax = plt.axes()
-ax.set_aspect('equal')
-graph = ax.pcolormesh(x_region, y_region, mandelbrot_array, cmap = 'twilight_shifted')
-plt.colorbar(graph)
-plt.xlabel("Real")
-plt.ylabel("Imaginary")
-plt.title('Mandelbrot set for $z_n$ = $z^2$ + c')
-plt.show()
+    # plot mandelbrot
+    ax = plt.axes()
+    ax.set_aspect('equal')
+    graph = ax.pcolormesh(x_region, y_region, mandelbrot_array, cmap = 'twilight_shifted')
+    plt.colorbar(graph)
+    plt.xlabel("Real")
+    plt.ylabel("Imaginary")
+    plt.title('Mandelbrot set for $z_n$ = $z^2$ + c')
+    plt.show()
--- Numpy_Vectorization/mandelbrot_mesh.py
+++ Numpy_Vectorization/mandelbrot_mesh.py
@@ -2,9 +2,9 @@ import numpy as np
 from matplotlib import pyplot as plt
 import time
 
-def compute_mandelbrot_grid(x_region, y_region, max_iterations, bound, power):
+def compute_mandelbrot_grid(x_mesh, y_mesh, max_iterations, bound, power):
     complex_number = 1j
-    C = x_region + y_region * complex_number
+    C = x_mesh + y_mesh * complex_number
     Z = np.zeros_like(C)
     M = np.zeros(C.shape, dtype=int)
 
@@ -23,23 +23,25 @@ x_res, y_res = 1024, 1024
 
 x_region = np.linspace(x_min, x_max, x_res)
 y_region = np.linspace(y_min, y_max, y_res)
-x_region, y_region = np.meshgrid(x_region, y_region)
+x_mesh, y_mesh = np.meshgrid(x_region, y_region)
 
 max_iterations = 100
 bound = 2
 power = 2
 
-# test time of computation
-start_time = time.perf_counter()
-mandelbrot_array = compute_mandelbrot_grid(x_region, y_region, max_iterations, bound, power)
-test_time = time.perf_counter() - start_time
-print(f'Computation took {test_time:.3f} seconds!')
+if __name__ == '__main__':
+    # test time of computation
+    start_time = time.perf_counter()
+    mandelbrot_array = compute_mandelbrot_grid(x_mesh, y_mesh, max_iterations, bound, power)
+    test_time = time.perf_counter() - start_time
+    print(f'Computation took {test_time:.4f} seconds!')
 
-ax = plt.axes()
-ax.set_aspect('equal')
-graph = ax.pcolormesh(x_region, y_region, mandelbrot_array, cmap = 'twilight_shifted')
-plt.colorbar(graph)
-plt.xlabel("Real")
-plt.ylabel("Imaginary")
-plt.title('Mandelbrot set for $z_n$ = $z^2$ + c')
-plt.show()
+    # plot mandelbrot
+    ax = plt.axes()
+    ax.set_aspect('equal')
+    graph = ax.pcolormesh(x_region, y_region, mandelbrot_array, cmap = 'twilight_shifted')
+    plt.colorbar(graph)
+    plt.xlabel("Real")
+    plt.ylabel("Imaginary")
+    plt.title('Mandelbrot set for $z_n$ = $z^2$ + c')
+    plt.show()
